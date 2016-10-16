57cbac29474bf2c5b2f1d6011796e20a6a562b32
khalilaitammar <ait.ammar.khalil@gmail.com>
2016-10-16 15:59:11 +0200

    [ADD] grid  3/3

--- js/app.js
+++ js/app.js
@@ -2,7 +2,24 @@ var app = angular.module('app', []);
 
 app.controller('TicTacToc', function($scope) {
 
+    $scope.bloc ={
+        top:[
+            {index:11, contenu:"", etat:null },
+            {index:12,contenu:"", etat:null },
+            {index:13,contenu:"", etat:null }
+        ],
+        middle:[
+            {index:21,contenu:"", etat:null },
+            {index:22,contenu:"", etat:null },
+            {index:23,contenu:"", etat:null }
+        ],
+        bottom:[
+            {index:31,contenu:"", etat:null },
+            {index:32,contenu:"", etat:null },
+            {index:33,contenu:"", etat:null }
+        ],
 
+    }
 
 
 
