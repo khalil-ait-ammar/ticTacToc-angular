4f802a2129dac46cdc23a35d0ac6c1ce9df91ffc
khalilaitammar <ait.ammar.khalil@gmail.com>
2016-10-16 16:01:30 +0200

    [ADD] symbole and player selection

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 var app = angular.module('app', []);
 
 app.controller('TicTacToc', function($scope) {
+    $scope.myVar = false;
+    $scope.toggle = function() {
+        $scope.myVar = !$scope.myVar;
+    };
 
     $scope.bloc ={
         top:[
@@ -21,7 +25,41 @@ app.controller('TicTacToc', function($scope) {
 
     }
 
+    $scope.playerActive = null;
+    $scope.symbol = null;
 
+    $scope.formData = {};
+
+    $scope.player= function (param) {
+        console.log( 'symbole select '+param, $scope.formData.symbol);
+        $scope.game=param;
+        if(!$scope.formData.symbol){
+            alert("select symbol");
+        }
+
+        $scope.playerActive = param;
+        console.log('active', $scope.playerActive);
+
+
+    }
+
+
+    $scope.clickGme = function (x,y,d) {
+
+        if ($scope.playerActive == null){
+            alert("select player");
+        }else{
+            if (x.etat == null){
+                console.log(y,d)
+                x.contenu = $scope.formData.symbol;
+                x.etat="clique";
+                $scope.formData.symbol =($scope.formData.symbol=='X') ? 'O':'X';
+
+            }
+
+        }
+
+    }
 
 
 });
